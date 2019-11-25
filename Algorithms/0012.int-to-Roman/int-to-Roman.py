3173b2ae7bd291fea793724b53ed5811ffafa67e
barrylll <904500917@qq.com>
2019-11-25 16:38:13 +0800

    new Solution(0012)

---
+++ Algorithms/0012.int-to-Roman/int-to-Roman.py
@@ -0,0 +1,17 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        nu = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        ch = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        temp = num
+        ret = ""
+        while temp > 0:
+            for i in range(len(ch)):
+                while temp - nu[i] >= 0:
+                    ret += ch[i]
+                    temp -= nu[i]
+        return ret
+
+if __name__ == '__main__':
+    print(
+        Solution().intToRoman(1994)
+    )
